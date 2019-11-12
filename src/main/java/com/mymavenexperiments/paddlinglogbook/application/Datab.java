03a75a7f0d2455779486a5d2e1922b9eecf567f3
leo <leonardo.sibani@gmail.com>
2019-11-12 12:53:32 +0100

    hard coded insert query now working. next step pass the user typed data to db

--- src/main/java/com/mymavenexperiments/paddlinglogbook/application/Datab.java
+++ src/main/java/com/mymavenexperiments/paddlinglogbook/application/Datab.java
@@ -28,7 +28,8 @@ public class Datab {
     public void addToTable(Descent d) {
         try {
             Statement addStatement = connect.createStatement();
-            addStatement.executeUpdate("insert into DESCENTS values (" + d.getId() + ", " + d.getAvgDifficulty() + ", " + d.getDate() + ", '" + d.getArea() + "', '" + d.getRiver() + "', '" + d.getStretch() + ");");
+            addStatement.executeUpdate("insert into descents values(2, 2, '15.11.2013', 'kerry', 'caragh', 'lower')");
+            //"insert into DESCENTS values (" + d.getId() + ", " + d.getAvgDifficulty() + ", " + d.getDate() + ", '" + d.getArea() + "', '" + d.getRiver() + "', '" + d.getStretch() + ");"
             addStatement.close();
         } catch (SQLException e) {
             System.out.println("Sorry, " + e);
